9bf140454e0394c073e8e0d4609ece0289b1e3cb
Kakarla PremChandu <premchandukakarla@gmail.com>
2024-09-20 18:16:43 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		for(int i = 0 ; i<4; i++){
        System.out.println("hello");
 	}
-
+	}
 }
